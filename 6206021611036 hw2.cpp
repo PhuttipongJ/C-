a980739ce8858c19bbd3efd696c9cae86bfad999
PhuttipongJ <77448573+PhuttipongJ@users.noreply.github.com>
2021-01-21 22:06:50 +0700

    Add files via upload

---
+++ 6206021611036 hw2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	string name;
+	int salary,sale,cp,tcp,total;
+	cout<<"-------------Homework 2-------------"<<endl;
+	cout<<"Enter Name : ";
+	cin>>name;
+	cout<<"Enter Salary : ";
+	cin>>salary;
+	cout<<"Enter Sale : ";
+	cin>>sale;
+	cout<<"Enter Commisstion Percent: ";
+	cin>>cp;
+	tcp=cp*sale/100;
+	total=tcp+salary;
+	cout<<"------------------------------------"<<endl;
+	cout<<"Your Name = "<<name<<endl;
+	cout<<"Total Revenue : "<<total<<" Bath"<<endl;
+	cout<<"------------------------------------"<<endl;
+	return 0;
+}
